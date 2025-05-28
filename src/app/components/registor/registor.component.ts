bfb03707bfb9894756fcc0b7bf0485f8871b0353
bhagirath lodhi <bhagirath@ags.local>
2025-05-28 15:35:24 +0530

    modified regiter ui

--- src/app/components/registor/registor.component.ts
+++ src/app/components/registor/registor.component.ts
@@ -3,21 +3,22 @@ import { CommonModule } from '@angular/common';
 import { FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule,FormsModule ,} from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-// import { Pipe } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { HttpClient } from '@angular/common/http';
-// import { FormsModule } from '@angular/forms';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
   selector: 'registor',
   standalone: true,
-  imports: [ReactiveFormsModule,FormsModule,CommonModule ],
+  imports: [ReactiveFormsModule,FormsModule,CommonModule, MatButtonModule, FontAwesomeModule],
   templateUrl: './registor.component.html',
   styleUrl: './registor.component.css'
 })
 export class RegistorComponent implements OnInit{
 // these field we are stroring into localStorage;
+  faAngleDown = faAngleDown;  
   userForm: FormGroup =new FormGroup({
     uName: new FormControl(""),
     eMail: new FormControl(""),
@@ -37,6 +38,7 @@ export class RegistorComponent implements OnInit{
 
  departments: any[] = [];
   onUserSave() {
+    debugger;
   const user = this.userForm.value;
   this.authService.signup(user);
   alert('Signup successful');
